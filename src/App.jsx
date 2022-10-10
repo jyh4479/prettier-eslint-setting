74f673381692a54d2477adfdaf0ae1667f2e22ae
jyh4479 <jyh.dev@gmail.com>
2022-10-10 20:31:56 +0900

    UPDATE PRETTIER CONFIG

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import { useState } from "react";
-import "./App.css";
-import Carousel from "./component/Carousel.jsx";
+import { useState } from 'react';
+import './App.css';
+import Carousel from './component/Carousel.jsx';
 
 function App() {
-  const [count, setCount] = useState(0);
+    const [count, setCount] = useState(0);
 
-  return (
-    <div className="App">
-      <Carousel />
-    </div>
-  );
+    return (
+        <div className='App'>
+            <Carousel />
+        </div>
+    );
 }
 
 export default App;
--- src/component/Carousel.jsx
+++ src/component/Carousel.jsx
@@ -1,13 +1,9 @@
-import React from "react";
+import React from 'react';
 
-/**
- * Carousel Test
- * @param {*} props Component Porps
- * @returns
- * */
-const Carousel = (props) => {
-  const testText = "123";
-  return <div>{testText}</div>;
+const Carousel = props => {
+    const testText = '123';
+    const testCharacter = 'a';
+    return <div>{testText}</div>;
 };
 
 export default Carousel;
